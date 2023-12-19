deda3dff616c3d39784aaf8649bb3d520bcd89d8
Akshay <akshaygupta089@gmail.com>
2023-12-19 21:05:47 +0530

    Ep6 useEffect hook and connecting to API

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,34 @@
 import RestaurantCard from './RestaurantCard';
-import resList from '../utils/mockData';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import Shimmer from './Shimmer';
 const Body = () => {
-  const [listOfRes, setListOfRes] = useState(resList);
-  return (
+  const [listOfRes, setListOfRes] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      'https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6529831&lng=77.20853939999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING'
+    );
+    const json = await data.json();
+    console.log(json);
+    setListOfRes(
+      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+  };
+
+  return listOfRes.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <button
         className="filter-btn"
         onClick={() => {
           const changedList = listOfRes.filter(
-            (item) => item.info.avgRating > 4.5
+            (item) => item.info.avgRating > 4.0
           );
           setListOfRes(changedList);
         }}
